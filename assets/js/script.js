4484b42336c85fcf789109fb28b6c48b7fc0dbe7
Charon3000505 <erlandidn@gmail.com>
2025-09-11 08:46:24 +0700

    clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,43 @@ const timer = setInterval(function () {
         document.getElementById("countdown").innerHTML = "<h2>🎉 Waktu Habis!</h2>";
     }
 }, 1000);
+
+
+const nav = document.querySelector('.nav-container');
+
+     window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+     });
+        
+      const navbar = document.querySelector('.nav-container');
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        });
+        
+        // Hamburger menu functionality
+        const hamburger = document.getElementById('hamburger');
+        const navLink = document.querySelector('.nav-link');
+        
+        hamburger.addEventListener('click', () => {
+            navLink.classList.toggle('active');
+        });
+
+        document.addEventListener("scroll", function() {
+    const aboutSection = document.querySelector(".about-container");
+    const sectionPosition = aboutSection.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if(sectionPosition < screenPosition) {
+        aboutSection.style.opacity = "1";
+        aboutSection.style.transform = "translateY(0)";
+    }
+});
+        
